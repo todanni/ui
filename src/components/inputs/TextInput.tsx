9e178e998ba3d563d8a29105b507147cc56a5630
Danni Popova <dannii.popova@gmail.com>
2023-04-23 15:26:48 +0100

    test: register input

--- src/components/inputs/TextInput.tsx
+++ src/components/inputs/TextInput.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
+import { UseFormRegister } from 'react-hook-form';
 
 const input = cva('block w-full rounded-lg border text-sm p-2.5', {
   variants: {
@@ -19,12 +20,16 @@ export interface TextInputProps
   extends React.InputHTMLAttributes<HTMLInputElement>,
     VariantProps<typeof input> {
   placeholder?: string;
+  register: UseFormRegister<any>;
+  name: string;
 }
 
 const TextInput: React.FC<TextInputProps> = ({
   className,
   intent,
   placeholder,
+  register,
+  name,
   ...props
 }) => (
   <input
@@ -32,6 +37,7 @@ const TextInput: React.FC<TextInputProps> = ({
     placeholder={placeholder}
     className={input({ className, intent })}
     {...props}
+    {...register(name)}
   />
 );
 
